f030c55e50ff3d55b4bea6b418f511f25638715a
Mahmoud-Nagy <mahmoud.naelhady@gmail.com>
2022-01-02 14:24:38 +0200

    Java Course All Section 7 1/2/2022

---
+++ src/java_course/sec_7_oop_composition/Wall.java
@@ -0,0 +1,13 @@
+package java_course.sec_7_oop_composition;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
---
+++ src/java_course/sec_7_oop_polymorphism/Mitsubishi.java
@@ -0,0 +1,22 @@
+package java_course.sec_7_oop_polymorphism;
+
+public class Mitsubishi extends Car{
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
