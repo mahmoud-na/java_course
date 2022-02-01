062f3e7d8dad3ff450b3bd791253cf6e4c2417d5
Mahmoud-Nagy <mahmoud.naelhady@gmail.com>
2022-02-01 16:01:06 +0200

    Interfaces challenge Done! 1/2/2022

---
+++ src/sec_9_inner_and_abstract_classes_and_interfaces/interfaces_challenge/ISaveable.java
@@ -0,0 +1,10 @@
+package sec_9_inner_and_abstract_classes_and_interfaces.interfaces_challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+
+    void read(List<String> savedValues);
+}
---
+++ src/sec_9_inner_and_abstract_classes_and_interfaces/javaaa.java
@@ -0,0 +1,4 @@
+package sec_9_inner_and_abstract_classes_and_interfaces;
+
+public class javaaa {
+}
